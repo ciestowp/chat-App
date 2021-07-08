5d8bd01b5f716c4ae4ac8ac6f67e756c8707b16f
USER <ciestowp@gmail.com>
2021-07-08 18:15:48 +0530

    commit 3:40 07-08-2021

--- app/src/main/java/com/example/mahabali/Constants.java
+++ app/src/main/java/com/example/mahabali/Constants.java
@@ -7,5 +7,4 @@ public class Constants {
     public static final String CHAT_SERVER_URL ="http://chat.socket.io";
     public static final String NEW_CHAT_SERVER_URL ="http://chat.socket.io";
     public static final String SOCKET_CHAT_SERVER_URL ="http://chat.socket.io";
-    public static final String VICKY_BRANCH_CHAT_SERVER_URL ="http://chat.socket.io";
 }
--- app/src/main/java/com/example/mahabali/socketiochat/AppContext.java
+++ app/src/main/java/com/example/mahabali/socketiochat/AppContext.java
@@ -34,7 +34,5 @@ public class AppContext extends Application {
         destroySocketListener();
     }
 
-    public void ok(){
 
-    }
 }
