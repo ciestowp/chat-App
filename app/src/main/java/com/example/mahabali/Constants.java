dcc68417104a79f2fa38585c6f45e387504003e6
USER <ciestowp@gmail.com>
2021-07-08 16:04:08 +0530

    Boolean added...

--- app/src/main/java/com/example/mahabali/Constants.java
+++ app/src/main/java/com/example/mahabali/Constants.java
@@ -4,10 +4,10 @@ package com.example.mahabali;
  * Created by Mahabali on 11/13/15.
  */
 public class Constants {
-    public static final String CHAT_SERVER_URL ="http://chat.socket.io";
-    public static final String URL_FOR_API_CALLING  ="http://chat.socket.io";
-    public static final String Vikram_changed  ="http://chat.socket.io";
-    public static final String Master_branch_changes_Add_Vikram_changed  ="http://chat.socket.io";
-
+    public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+    public static final String URL_FOR_API_CALLING = "http://chat.socket.io";
+    public static final String Vikram_changed = "http://chat.socket.io";
+    public static final String Master_branch_changes_Add_Vikram_changed = "http://chat.socket.io";
+    private final Boolean isValid = true;
 
 }
