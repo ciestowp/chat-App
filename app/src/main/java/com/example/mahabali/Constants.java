a1eb81ef40f0715d309188d7321bbc9e31c633e5
USER <ciestowp@gmail.com>
2021-07-08 16:09:38 +0530

    alpesh changed code

--- app/src/main/java/com/example/mahabali/Constants.java
+++ app/src/main/java/com/example/mahabali/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
     public static final String URL_FOR_API_CALLING = "http://chat.socket.io";
     public static final String Vikram_changed = "http://chat.socket.io";
     public static final String Master_branch_changes_Add_Vikram_changed = "http://chat.socket.io";
+    public static final String Alpesh_branch_changes_Add_Vikram_changed = "http://chat.socket.io";
     private final Boolean isValid = true;
 
 }
